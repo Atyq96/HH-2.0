c477f93169b333757419077b3dfa244808647408
Amine Atyq <atyqamine1996@gmail.com>
2024-07-05 19:06:05 +0100

    fix2

--- spotify-app/src/components/navbar.tsx
+++ spotify-app/src/components/navbar.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import  { useState, useEffect, useRef } from "react";
 import { FaBars } from "react-icons/fa";
 import { IoCloseSharp } from "react-icons/io5";
 
